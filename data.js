91dae87b2253a2cb147514ba8195e74844ddcd1e
song <imeoer@gmail.com>
2014-11-23 22:36:17 +0800

    add some word

--- data.js
+++ data.js
@@ -31,7 +31,62 @@ var data = [
                 "zh": "不好意思",
                 "ro": "Su mi ma sen",
                 "jp": "すみません"
+            },
+            {
+                "zh": "多少钱?",
+                "ro": "I ku ra de su ka",
+                "jp": "いくらですか"
+            },
+            {
+                "zh": "稍等一下",
+                "ro": "Cho tto ma tte",
+                "jp": "ちょっと待って"
+            },
+            {
+                "zh": "我不懂",
+                "ro": "Wa ka ri ma sen",
+                "jp": "分かりません"
+            },
+            {
+                "zh": "拜托了",
+                "ro": "O ne ga i shi ma su",
+                "jp": "お願いします"
+            },
+            {
+                "zh": "不要紧、没关系",
+                "ro": "Da i joo bu",
+                "jp": "大丈夫"
+            },
+            {
+                "zh": "再一次",
+                "ro": "Moo i chi do",
+                "jp": "もう一度"
+            },
+            {
+                "zh": "是的",
+                "ro": "Soo de su",
+                "jp": "そうです"
             }
         ]
+    },
+    {
+        name: "购物",
+        list: []
+    },
+    {
+        name: "住宿",
+        list: []
+    },
+    {
+        name: "街牌",
+        list: []
+    },
+    {
+        name: "交通",
+        list: []
+    },
+    {
+        name: "动漫",
+        list: []
     }
 ];
